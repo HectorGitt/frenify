219fb355629f70d2ec7e843b25abfd5d2f796e03
Hector <adeniyi.olaitanhector@yahoo.com>
2023-03-18 17:45:35 +0100

    added home and blogpost views

--- frenifyBlog/views.py
+++ frenifyBlog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+
+from .models import BlogPost
 
 # Create your views here.
+
+
+
+def home(request):
+    blog_posts = BlogPost.objects.all()
+    return render(request, 'frenifyBlog/index.html', {'blog_posts': blog_posts})
+
+class BlogDetailView(DetailView):
+    model = BlogPost
+    template_name = 'frenifyBlog/blog_post.html'
