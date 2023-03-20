2112198fae86896da86dc1c4cde82f4db5038b27
Hector <adeniyi.olaitanhector@yahoo.com>
2023-03-20 16:13:20 +0100

    added twitteruser models

--- frenifyBlog/models.py
+++ frenifyBlog/models.py
@@ -11,14 +11,38 @@ import shortuuid
 
 # Create your models here.
 
+class TwitterAuthToken(models.Model):
+    oauth_token = models.CharField(max_length=255)
+    oauth_token_secret = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.oauth_token
+
+class TwitterUser(models.Model):
+    twitter_id = models.CharField(max_length=255, unique=True)
+    screen_name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    profile_image_url = models.CharField(max_length=255, null=True)
+    twitter_oauth_token = models.ForeignKey(TwitterAuthToken, on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    followers = models.PositiveIntegerField(default=None, null=True, blank=True)
+    account_months = models.PositiveIntegerField(default=None, null=True, blank=True)
+    email = models.CharField(max_length=255, null=True, blank=True)
+    eth_wallet_id = models.CharField(max_length=42, null=True, blank=True)
+    sol_wallet_id = models.CharField(max_length=44, null=True, blank=True)
+    
+    def __str__(self):
+        return self.screen_name
+
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return str(self.name)
     
-
-category = Category.objects.all().first()
+    @classmethod
+    def get_default_pk(cls):
+        return cls.objects.first().pk
         
 class BlogPost(models.Model):
     twitter = models.CharField(max_length=255, null=True, blank=True, help_text='@username')
@@ -27,9 +51,10 @@ class BlogPost(models.Model):
     thumbnail = ImageField(upload_to='media/uploads/', blank=True, null=True)
     description = models.TextField(blank=True, help_text='Leave blank to populate from twitter profile')
     content = RichTextUploadingField(config_name='awesome_ckeditor', blank=True, null=True)
-    likes = models.IntegerField(default=0, editable=False)
+    default_likes = models.IntegerField(default=0, editable=False)
     date = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=category, help_text='Verify this is the same as the category name in the blog post')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=Category.get_default_pk, help_text='Verify this is the same as the category name in the blog post')
+    liked_by = models.ManyToManyField(TwitterUser, related_name='liked_by', blank=True)
 
     def __str__(self):
         return str(self.title)
